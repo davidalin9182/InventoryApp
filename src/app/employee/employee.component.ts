1c55563f371c08b80e1dfca59cab97324bee34f5
David Alin <davidalin9182@gmail.com>
2023-07-31 00:34:15 +0300

    second_commit

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RoomsService } from '../services/rooms.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
+  //to crate separate insatnce for the employee component not that used
+  providers: [RoomsService]
 })
 export class EmployeeComponent {
   empName: string = 'Employe1';
-
+  constructor(private roomService: RoomsService){
+    
+  }
 }
